79a1ea5dd47b95459f73ee4b9d4f1ea3a760708c
kmshyn76 <147911990+Ardhi27@users.noreply.github.com>
2024-09-12 23:10:54 +0800

    Add remote

--- test.mobile/ContentView.swift
+++ test.mobile/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    var highPriority = ["Wawancara", "Design Science Research"]
+    var mediumPriority = ["Desain UI", "Menambahkan Kolom"]
+    var lowPriority = ["Buat Content"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        VStack(alignment: .leading){
+            Text("To-Do List").font(.system(size: 20, weight: .bold)).italic()
+            HStack(){
+                GroupBox(label: Text("High Priority").foregroundColor(Color.red)){
+                    ScrollView(.vertical, showsIndicators: true){
+                        VStack(alignment: .leading, spacing: 5){
+                            ForEach(highPriority, id: \.self){
+                                task in
+                                Text(task).foregroundColor(Color.white)
+                            }
+                        }
+                    }
+                }.backgroundStyle(Color.black)
+                GroupBox(label: Text("Medium Priority").foregroundColor(Color.yellow)){
+                    ScrollView(.vertical, showsIndicators: true){
+                        VStack(alignment: .leading, spacing: 5){
+                            ForEach(mediumPriority, id: \.self){
+                                task in
+                                Text(task).foregroundColor(Color.white)
+                            }
+                        }
+                    }
+                }.backgroundStyle(Color.black)
+            }
+            HStack(){
+                GroupBox(label: Text("Low Priority").foregroundColor(Color.green)){
+                    ScrollView(.vertical, showsIndicators: true){
+                        VStack(alignment: .leading, spacing: 10){
+                            ForEach(lowPriority, id:\.self){
+                                task in
+                                Text(task).foregroundColor(Color.white)
+                            }
+                        }
+                    }
+                }.backgroundStyle(Color.black)
+            }
+        }.padding(10)
     }
 }
 
