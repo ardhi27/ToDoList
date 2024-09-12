b683cfc3aaff94cae383bb4dffd5ad28c4285e10
kmshyn76 <147911990+Ardhi27@users.noreply.github.com>
2024-09-12 16:33:04 +0800

    Initial Commit

---
+++ test.mobile/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  test.mobile
+//
+//  Created by Agung Ardhiyanda on 12/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test.mobile/test_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_mobileApp.swift
+//  test.mobile
+//
+//  Created by Agung Ardhiyanda on 12/09/24.
+//
+
+import SwiftUI
+
+@main
+struct test_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
