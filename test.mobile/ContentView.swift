723e583416fe2f0703406a373f26f6d94dcf415e
kmshyn76 <147911990+Ardhi27@users.noreply.github.com>
2024-09-13 16:29:44 +0800

    Add modal and function to add task

--- test.mobile/ContentView.swift
+++ test.mobile/ContentView.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    var highPriority = ["Wawancara", "Design Science Research"]
-    var mediumPriority = ["Desain UI", "Menambahkan Kolom"]
-    var lowPriority = ["Buat Content"]
+    @State var highPriority: [String] = []
+    @State var mediumPriority: [String] = []
+    @State var lowPriority: [String] = []
+    @State private var showModal = false
+    
     var body: some View {
-        VStack(alignment: .leading){
+        VStack{
+            VStack(alignment: .leading){
             Text("To-Do List").font(.system(size: 20, weight: .bold)).italic()
             HStack(){
                 GroupBox(label: Text("High Priority").foregroundColor(Color.red)){
@@ -48,7 +51,62 @@ struct ContentView: View {
                     }
                 }.backgroundStyle(Color.black)
             }
-        }.padding(10)
+            }.padding(10)
+            VStack(alignment: .trailing){
+                Button("Add List"){
+                    showModal.toggle()
+            
+                }
+                .sheet(isPresented: $showModal){
+                    ModalView{listName, selectedPriority in
+                        addTask(listName: listName, priority: selectedPriority)
+                    }
+                }
+            }
+        }
+    }
+    
+    private func addTask(listName: String, priority: String){
+        switch priority {
+        case "High Priority" :
+            highPriority.append(listName)
+        case "Medium Priority" :
+            mediumPriority.append(listName)
+        case "Low Priority" :
+            lowPriority.append(listName)
+        default:
+            break
+        }
+    }
+}
+
+struct ModalView: View{
+    @Environment(\.dismiss) var dismiss
+    @State private var listName: String = ""
+    @State private var selectedPriority: String = "High Priority"
+    @State private var listPriority: Array = ["High Priority", "Low Priority", "Medium Priority"]
+    
+    var onAddTask: (String, String) -> Void
+    
+    var body: some View{
+        VStack(){
+            Text("Test Modal")
+            TextField("Input Your List Today",
+                      text: $listName).padding(4).overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.black, lineWidth: 2))
+                Picker("Priority Task", selection: $selectedPriority){
+                    ForEach(listPriority, id:\.self){
+                        priority in
+                        Text(priority).tag(priority)
+                    }
+                }
+            
+            Spacer()
+            
+            Button("Add Modal"){
+                onAddTask(listName, selectedPriority)
+                dismiss()
+            }
+        }.padding(20)
     }
 }
 
